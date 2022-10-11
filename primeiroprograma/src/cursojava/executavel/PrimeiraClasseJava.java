42fab51ed38cbfb9d484ae725a7c81544b64b426
adils <adils@DESKTOP-NJ39LIV>
2022-10-11 01:11:55 -0300

    e2

--- primeiroprograma/src/cursojava/executavel/PrimeiraClasseJava.java
+++ primeiroprograma/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -5,6 +5,7 @@ public class PrimeiraClasseJava {
 	public static void main(String[] args) {
 		System.out.println("Meu primeiro programa em Java");
 		System.out.println("Java é orientado a objetos");
+		/*teste*/
 	}
 
 }
