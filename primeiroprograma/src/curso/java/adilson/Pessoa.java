722b6cfb975ceec0be6368dfe624c6adf58cc55d
adils <adils@DESKTOP-NJ39LIV>
2022-10-11 04:47:42 -0300

    e

--- primeiroprograma/src/curso/java/adilson/Pessoa.java
+++ primeiroprograma/src/curso/java/adilson/Pessoa.java
@@ -2,4 +2,19 @@ package curso.java.adilson;
 
 public class Pessoa {
 
+		private String nome;
+		private String sobrenome;
+		public String getNome() {
+			return nome;
+		}
+		public void setNome(String nome) {
+			this.nome = nome;
+		}
+		public String getSobrenome() {
+			return sobrenome;
+		}
+		public void setSobrenome(String sobrenome) {
+			this.sobrenome = sobrenome;
+		}
+		
 }
